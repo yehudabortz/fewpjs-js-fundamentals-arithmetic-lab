aa851a2e5f72e06c176757f6d8af4e641d658948
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-22 16:48:48 -0500

    commit tree 23a0cac5d83700e6d4abf70660d727231b9f606a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random())
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4
+
+
+let max = Math.max(1,2,4,5,6,10,20)
